eceaf4ddc5eaf9615019da5112a81ecd4123da8c
github username <sharnellgrant@gmail.com>
2021-06-02 15:45:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+let main = document.querySelector('main#main')
+  main.remove()
+
+  const newHeader = document.createElement('h1');
+  newHeader.id = 'victory'
+  newHeader.innerHTML = "Sharnell is the champion!"
